e3362c3be10370e1e4ab6f13e7e88233568bf142
ssmith1975 <ssmith1975.soc@gmail.com>
2018-04-23 04:43:11 -0500

    Init commit

---
+++ RedirectHTTPHandler/RedirectHTTPHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Web;
+
+namespace RedirectHTTPHandler
+{
+    public class RedirectHTTPHandler:IHttpHandler
+    {
+
+        public bool IsReusable {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            // Should redirect to homepage
+            context.Response.Redirect("~");
+        }
+
+
+    }
+}
---
+++ RedirectHTTPHandler/RedirectHTTPHandlerFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace RedirectHTTPHandler
+{
+    public class RedirectHTTPHandlerFactory : System.Web.UI.PageHandlerFactory
+    {
+        public override  IHttpHandler GetHandler(HttpContext context, string requestType, string url, string pathTranslated)
+        {
+            string localHost;
+            string referrerHost;
+
+            localHost = context.Request.Url.Host;
+            referrerHost = (context.Request.UrlReferrer != null ? context.Request.UrlReferrer.Host : String.Empty);
+
+            // Is request local?
+            if (localHost == referrerHost)
+            {
+                // Business as usual
+                return base.GetHandler(context, requestType, url, pathTranslated);//new DefaultHttpHandler();
+            }
+            else
+            { 
+                // Intercept invalid page request
+                return new RedirectHTTPHandler();
+                //context.Response.Write("Not a local request! Get outta here!!!<br/><br/>");
+
+            }
+           
+        }
+
+        //public void ReleaseHandler(IHttpHandler handler)
+        //{
+            //Do nothing
+            //throw new NotImplementedException();
+        //}
+    }
+}
